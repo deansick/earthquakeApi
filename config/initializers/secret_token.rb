1633d223e764e0335a4e22c238d39687b5fbcad0
Sean Dick <sean@seandick.net>
2014-03-14 20:31:47 -0700

    forgot to uncomment the dev secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,5 +9,5 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-# EarthquakeAPI::Application.config.secret_token = ENV['SECRET_TOKEN']
-EarthquakeAPI::Application.config.secret_key_base = '58126cf5c1f3599c96236c2203aaee170151dfab46c71efd44d6c824ecade4b9dc2cc78715f67dc7033904e05f649df6e229fe121af91175b8a7c8aa78901af9'
+EarthquakeAPI::Application.config.secret_token = ENV['SECRET_TOKEN']
+# EarthquakeAPI::Application.config.secret_key_base = '58126cf5c1f3599c96236c2203aaee170151dfab46c71efd44d6c824ecade4b9dc2cc78715f67dc7033904e05f649df6e229fe121af91175b8a7c8aa78901af9'
